b1f722e251be32efc247343a3b561b9fbf2b70e9
Nikita Starshinov <nstarshinov@yandex.ru>
2024-04-01 16:03:41 +0300

    Add Optional+orThrow.swift

---
+++ Optional+orThrow.swift
@@ -0,0 +1,21 @@
+
+extension Optional {
+    
+    func orThrow<E: Error>(_ error: @autoclosure () -> E) throws -> Wrapped {
+        guard let self else {
+            throw error()
+        }
+        return self
+    }
+    
+    struct NilError: Error {
+        let type = Wrapped.self
+    }
+    
+    func orThrow() throws -> Wrapped {
+        guard let self else {
+            throw NilError()
+        }
+        return self
+    }
+}
